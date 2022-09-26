9d51154237a3966197da2fe816282538f3936fae
Arthur Galdino <arthurgaldinoo@hotmail.com>
2022-09-26 20:02:55 -0300

    Adicionando Saldo na carteira

--- src/components/CarteiraVirtual.vue
+++ src/components/CarteiraVirtual.vue
@@ -1,33 +1,71 @@
 <template>
   <div>
-    <h4 class="titulo-carteira">Carteira Virtual</h4>
-    <p class="border border-success rounded">R$ = 100,00</p>
+    <div class="cadastra-saldo">
+      <div class="form-group">
+        <label for="exampleInputPassword1">Diga seu Saldo</label>
+        <div class="saldo-botao">
+          <input
+            type="text"
+            class="form-control border border-success rounded"
+            id="exampleInputPassword1"
+            v-model="saldo.valor"
+          />
+          <button type="button" class="btn btn-outline-success btn-sm">Enviar</button>
+
+
+        </div>
+      </div>
+    </div>
+    <div class="carteira">
+      <h4 class="titulo-carteira">Carteira Virtual</h4>
+      <p class="border border-success rounded">R$ = {{saldo.valor}} </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:"CarteiraVirtual",
-
-}
+  name: "CarteiraVirtual",
+  data(){
+    return{
+        saldo :{
+            valor:0
+        }
+    }
+  }
+};
 </script>
 
 <style scoped>
-    div{
-        color: aliceblue;
-        margin-top: 600px;
-    }
+.cadastra-saldo {
+  margin: 50px auto 5px auto;
+  color: aliceblue;
+  width: 70%;
+  height: 50%;
+}
 
-    .titulo-carteira{
-        padding: 10px;
-        font-size: 18px;
-        margin-bottom: 10px;
-    }
+.saldo-botao{
+    display: flex;
+    justify-content: space-around;
+    
+}
 
-    p{
-        width: 80%;
-        margin: auto;
-        padding-bottom: 3px;
-        padding-top: 3px;
-    }
+.carteira {
+  margin-top: 30px;
+}
+
+.titulo-carteira {
+  color: aliceblue;
+  padding: 10px;
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+p {
+  color: aliceblue;
+  width: 80%;
+  margin: auto;
+  padding-bottom: 3px;
+  padding-top: 3px;
+}
 </style>
